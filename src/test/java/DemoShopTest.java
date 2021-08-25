125c91a00498cc2c61aba3c63cd8a3e2f9761d17
unknown <Sucala_adrian28@yahoo.com>
2021-08-25 19:59:15 +0300

    testing accond button from home page

--- src/test/java/DemoShopTest.java
+++ src/test/java/DemoShopTest.java
@@ -3,7 +3,9 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.sleep;
 
 public class DemoShopTest {
 
@@ -18,5 +20,12 @@ public class DemoShopTest {
     public void I_expect_a_welcome_message() {
         SelenideElement welcomeMessage = $(".welcome-msg");
         welcomeMessage.shouldHave(text("Welcome "));
+    }
+    @Test
+    public void accountButtonHommePage(){
+        SelenideElement verrifyMainPageAccount = $(".account-cart-wrapper .skip-account");
+        verrifyMainPageAccount.click();
+        verrifyMainPageAccount.shouldBe(visible);
+        sleep(3000);
     }
 }
